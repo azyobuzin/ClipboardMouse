d76adb466f4f3e76c77cbac4984c9203c5443008
azyobuzin <azyobuzin@users.sourceforge.jp>
2012-07-12 23:21:37 +0900

    Initial commit

---
+++ ClipboardMouse/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ClipboardMouse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mouseTimer = new System.Windows.Forms.Timer(this.components);
+            this.clipboardTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(1);
+            this.label1.Size = new System.Drawing.Size(114, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "[Nothing or not text]";
+            this.label1.SizeChanged += new System.EventHandler(this.label1_SizeChanged);
+            // 
+            // mouseTimer
+            // 
+            this.mouseTimer.Enabled = true;
+            this.mouseTimer.Interval = 50;
+            this.mouseTimer.Tick += new System.EventHandler(this.mouseTimer_Tick);
+            // 
+            // clipboardTimer
+            // 
+            this.clipboardTimer.Enabled = true;
+            this.clipboardTimer.Interval = 1;
+            this.clipboardTimer.Tick += new System.EventHandler(this.clipboardTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Fuchsia;
+            this.ClientSize = new System.Drawing.Size(114, 16);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.65D;
+            this.ShowInTaskbar = false;
+            this.Text = "ClipboardMouse";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer mouseTimer;
+        private System.Windows.Forms.Timer clipboardTimer;
+    }
+}
+
---
+++ ClipboardMouse/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ClipboardMouse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_SizeChanged(object sender, EventArgs e)
+        {
+            this.Size = label1.Size;
+        }
+
+        private void mouseTimer_Tick(object sender, EventArgs e)
+        {
+            this.Location = Point.Add(Cursor.Position, new Size(20, 20));
+        }
+
+        private void clipboardTimer_Tick(object sender, EventArgs e)
+        {
+            clipboardTimer.Interval = 1000;
+            if (Clipboard.ContainsText())
+            {
+                label1.Text = Clipboard.GetText();
+            }
+            else
+            {
+                label1.Text = "[Nothing or not text]";
+            }
+        }
+    }
+}
